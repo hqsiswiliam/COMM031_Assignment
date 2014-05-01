62070ffc75a73032b5fd248281c08229b6d78920
hqsiswiliam <hqsiswiliam@gmail.com>
2014-05-02 00:14:48 +0100

    Explain how to search via tweepy

--- tweepy_credential_test.py
+++ tweepy_credential_test.py
@@ -1,9 +1,9 @@
 import tweepy
  
 # Consumer keys and access tokens, used for OAuth
-consumer_key = ''
+consumer_key = '1y4WdNqMIpNYkAn1nxaXK7eXo'
 consumer_secret = ''
-access_token = '2231668399-'
+access_token = '2231668399-fbqTk5yqUjdA7owjGWL4knuzaW5V5TpklzyL3ag'
 access_token_secret = ''
  
 # OAuth process, using the keys and tokens
@@ -12,6 +12,7 @@ auth.set_access_token(access_token, access_token_secret)
  
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
- 
-# Sample method, used to update a status
-api.update_status('Hello from Qiushi Huang!')
+
+results = api.search(q='2012',lang='zh',result_type='mix',count='450')
+for result in results:
+	print result.text.encode('utf8')
